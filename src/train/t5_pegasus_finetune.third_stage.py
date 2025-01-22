b03d4c67ee34ab43a23a3b9078a491401138be7e
chenyongming <nealchan@buaa.edu.cn>
2025-01-22 11:01:59 +0800

    a

--- src/train/t5_pegasus_finetune.third_stage.py
+++ src/train/t5_pegasus_finetune.third_stage.py
@@ -3,7 +3,7 @@
 #
 #    @Create Author : chenyongming
 #    @Create Time   : 2025-01-02 15:37
-#    @Description   : T5 PEGASUS Finetune torch脚本 fine-grained第三阶段细粒度
+#    @Description   : T5 PEGASUS Finetune第三阶段 torch脚本
 #
 # ===============================================================
 
@@ -21,35 +21,29 @@ from utils import log
 from utils import time_util
 from bert4torch.models import *
 from torch.utils.data import DataLoader, Dataset
+# from torch._six import container_abcs, string_classes, int_classes
 from collections.abc import Mapping, Sequence, Container
 string_classes = (str,)
 int_classes = (int,)
 from transformers import MT5ForConditionalGeneration, BertTokenizer
-import random
 
 
-
-# 项目根目录
-ROOT_DIR = osp.dirname(osp.dirname(osp.dirname(osp.abspath(__file__))))
-DATA_PATH = osp.join(ROOT_DIR, 'data', 'torch_data')
+ROOT_DIR = osp.dirname(osp.dirname(osp.dirname(osp.abspath(__file__))))  # 项目根目录
+DATA_PATH = osp.join(ROOT_DIR, 'data', 'THUCNews')
 MODEL_SAVE_PATH = osp.join(ROOT_DIR, 'model')
 MODEL_SPECIFIC_PATH = 't5_pegasus'
 PRETRAIN_MODEL_PATH = osp.join(ROOT_DIR, 'model', 'chinese_t5_pegasus_base_torch')
 LOG_DIR = osp.join(ROOT_DIR, "logs")  # 日志目录
-STAGE_MAP = {
-    'third_stage': 'second_stage',
-    'second_stage': 'first_stage',
-    'first_stage': 'pretrain'
-}
 
 
 def load_data(filename):
     """加载数据
-    单条格式：(标题, 正文)
+    单条格式：(标题, 正文, 增强正文)
     """
     D = []
     with open(filename, encoding='utf-8') as f:
         for l in f.readlines():
+            # cur = l.strip().split('\t')
             cur = l.strip().split('\u0001')
             if len(cur) == 2:
                 title, content = cur[0], cur[1]
@@ -58,8 +52,8 @@ def load_data(filename):
                 content = cur[0]
                 D.append(content)
             elif len(cur) == 3:
-                title, augmented_text, content = cur[0], cur[1], cur[2]
-                D.append((title, content, augmented_text))
+                title, content, augment_content = cur[0], cur[1], cur[2]
+                D.append((title, content, augment_content))
     return D
 
 
@@ -93,28 +87,20 @@ class KeyDataset(Dataset):
         return self.data[index]
 
 
-def create_data(stage, data, tokenizer, max_len=512, term='train'):
+def create_data(data, tokenizer, max_len=512, term='train'):
     """调用tokenizer.encode编码正文/标题，每条样本用dict表示数据域
-       增加了读取增强数据的功能
     """
     ret, flag = [], True
-    for item in data:
-        # print(f'Length of item: {len(item)}')
-        if args.stage == 'third_stage' and len(item) == 3:  # 第三阶段，数据格式：(标题, 正文, 增强文本)
-            title, content, augmented_text = item
-            augmented_ids = tokenizer.encode(augmented_text, max_length=max_len, truncation='only_first')
-        else:
-            title, content = item
-            augmented_ids = None  # 第一、二阶段没有增强数据
+    for title, content in data:
         text_ids = tokenizer.encode(content, max_length=max_len, truncation='only_first')
+        print(f'length of text_ids: {len(text_ids)}')
         if term == 'train':
             summary_ids = tokenizer.encode(title, max_length=max_len, truncation='only_first')
+            print(f'length of summary_ids: {len(summary_ids)}')
             features = {'input_ids': text_ids,
                         'decoder_input_ids': summary_ids,
                         'attention_mask': [1] * len(text_ids),
-                        'decoder_attention_mask': [1] * len(summary_ids),
-                        'content': content,
-                        'augmented_data': {'input_ids': augmented_ids, 'attention_mask': [1] * len(augmented_ids)} if augmented_ids else None
+                        'decoder_attention_mask': [1] * len(summary_ids)
                         }
 
         elif term == 'dev':
@@ -157,6 +143,8 @@ def default_collate(batch):
     if isinstance(elem, torch.Tensor):
         out = None
         if torch.utils.data.get_worker_info() is not None:
+            # If we're in a background process, concatenate directly into a
+            # shared memory tensor to avoid an extra copy
             numel = sum([x.numel() for x in batch])
             storage = elem.storage()._new_shared(numel)
             out = elem.new(storage)
@@ -164,6 +152,7 @@ def default_collate(batch):
     elif elem_type.__module__ == 'numpy' and elem_type.__name__ != 'str_' \
             and elem_type.__name__ != 'string_':
         if elem_type.__name__ == 'ndarray' or elem_type.__name__ == 'memmap':
+            # array of string classes and object
             if np_str_obj_array_pattern.search(elem.dtype.str) is not None:
                 raise TypeError(default_collate_err_msg_format.format(elem.dtype))
             return default_collate([torch.as_tensor(b) for b in batch])
@@ -180,6 +169,7 @@ def default_collate(batch):
     elif isinstance(elem, tuple) and hasattr(elem, '_fields'):  # namedtuple
         return elem_type(*(default_collate(samples) for samples in zip(*batch)))
     elif isinstance(elem, Sequence):
+        # check to make sure that the elements in batch have consistent size
         it = iter(batch)
         elem_size = len(next(it))
         if not all(len(elem) == elem_size for elem in it):
@@ -192,7 +182,7 @@ def prepare_data(args, data_path, tokenizer, term='train'):
     """准备batch数据
     """
     data = load_data(data_path)
-    data = create_data(args.stage, data, tokenizer, args.max_len, term)
+    data = create_data(data, tokenizer, args.max_len, term)
     data = KeyDataset(data)
     data = DataLoader(data, batch_size=args.batch_size, collate_fn=default_collate)
     return data
@@ -231,19 +221,9 @@ def compute_rouges(sources, targets):
     return {k: v / len(targets) for k, v in scores.items()}
 
 
-class ContrastiveLoss(nn.Module):
-    def __init__(self, temperature=0.1):
-        super().__init__()
-        self.temperature = temperature
-
-    def forward(self, z_i, z_j_all, labels):
-        sim = torch.bmm(z_i.unsqueeze(1), z_j_all.transpose(1, 2)) / self.temperature
-        sim = sim.squeeze(1)
-        loss = F.cross_entropy(sim, labels)
-        return loss
-
-
-def train_model(model, adam, train_data, dev_data, tokenizer, device, args, contrastive_loss):
+def train_model(model, adam, train_data, dev_data, tokenizer, device, args):
+    # if not os.path.exists(args.model_dir):
+    #     os.mkdir(args.model_dir)
     model_save_path = os.path.join(args.model_dir, args.model_specific_dir)
     if not os.path.exists(model_save_path):
         os.mkdir(model_save_path)
@@ -253,21 +233,19 @@ def train_model(model, adam, train_data, dev_data, tokenizer, device, args, cont
         model.train()
         for i, cur in enumerate(tqdm(train_data, desc='Epoch {}:'.format(epoch))):
             cur = {k: v.to(device) for k, v in cur.items()}
-            # 对比学习
-            aug_data = cur['augmented_data']
-            z_i = model(**cur)[0].last_hidden_state[:, 0, :]
-            z_j_all = torch.cat([model(**aug_data)[0].last_hidden_state[:, 0, :], z_i], dim=0)
-            labels = torch.cat([torch.ones(len(z_i)), torch.zeros(len(z_i))], dim=0)
-            contrastive_l = contrastive_loss(z_i, z_j_all, labels)
-
+            print(f'cur: {cur}')
+            print(f'cur["input_ids"] shape: {cur["input_ids"].shape}')
             prob = model(**cur)[0]
+            print(f'prob: {prob}')
+            print(f'prob shape: {prob.shape}')
             mask = cur['decoder_attention_mask'][:, 1:].reshape(-1).bool()
             prob = prob[:, :-1]
+            print(f'prob step 2 shape: {prob.shape}')
             prob = prob.reshape((-1, prob.size(-1)))[mask]
-            labels_ce  = cur['decoder_input_ids'][:, 1:].reshape(-1)[mask]
+            print(f'prob step 3 shape: {prob.shape}')
+            labels = cur['decoder_input_ids'][:, 1:].reshape(-1)[mask]
             loss_fct = torch.nn.CrossEntropyLoss(ignore_index=-100)
-
-            loss = loss_fct(prob, labels_ce) + contrastive_l * 0.1
+            loss = loss_fct(prob, labels)
             if i % 100 == 0:
                 log.logger.info("Iter {}:  Training Loss: {}".format(i, loss.item()))
             loss.backward()
@@ -294,6 +272,8 @@ def train_model(model, adam, train_data, dev_data, tokenizer, device, args, cont
                                      **content)
             gen = tokenizer.batch_decode(gen, skip_special_tokens=True)
             gen = [item.replace(' ', '') for item in gen]
+            # print(title)
+            # print(gen)
             gens.extend(gen)
             summaries.extend(title)
         scores = compute_rouges(gens, summaries)
@@ -305,25 +285,27 @@ def train_model(model, adam, train_data, dev_data, tokenizer, device, args, cont
                 torch.save(model.module, os.path.join(model_save_path, args.stage + '_' + args.version))
             else:
                 torch.save(model, os.path.join(model_save_path, args.stage + '_' + args.version))
+        # torch.save(model, os.path.join(args.model_dir, 'summary_model_epoch_{}'.format(str(epoch))))
 
 
 def init_argument():
     parser = argparse.ArgumentParser(description='t5-pegasus-chinese')
-    parser.add_argument('--train_data', default=osp.join(DATA_PATH, 'train_data.tsv'))
-    parser.add_argument('--dev_data', default=osp.join(DATA_PATH, 'dev.tsv'))
+    parser.add_argument('--train_data', default=osp.join(DATA_PATH, 'companies_news_info_v2.valid.txt'))
+    parser.add_argument('--dev_data', default=osp.join(DATA_PATH, 'companies_news_info_v2.valid.txt'))
     parser.add_argument('--pretrain_model', default=PRETRAIN_MODEL_PATH)
     parser.add_argument('--model_dir', default=osp.join(MODEL_SAVE_PATH, 'saved_model'))
     parser.add_argument('--model_specific_dir', default=MODEL_SPECIFIC_PATH)
 
     parser.add_argument('--num_epoch', type=int, default=20, help='number of epoch')
-    parser.add_argument('--batch_size', type=int, default=16, help='batch size')
+    parser.add_argument('--batch_size', type=int, default=2, help='batch size')
     parser.add_argument('--lr', type=float, default=2e-4, help='learning rate')
     parser.add_argument('--data_parallel', default=False)
     parser.add_argument('--max_len', type=int, default=512, help='max length of inputs')
     parser.add_argument('--max_len_generate', type=int, default=40, help='max length of outputs')
     parser.add_argument('--length_penalty', type=float, default=1.2, help='higher penalty causes longer summary')
     parser.add_argument('--version', type=str, default='v1', help='version')
-    parser.add_argument('--stage', type=str, default='third_stage', help='training stages')
+    parser.add_argument('--stage', type=str, default='one_stage',
+                        choices=['pretrain', 'one_stage', 'two_stage'], help='training stage')
 
     args = parser.parse_args()
     return args
@@ -335,7 +317,6 @@ def _log_args():
     for k, v in args.__dict__.items():
         log.logger.debug(f'{k}: {v}')
     log.logger.debug('')
-
 
 
 if __name__ == '__main__':
@@ -354,10 +335,13 @@ if __name__ == '__main__':
     train_data = prepare_data(args, args.train_data, tokenizer, term='train')
     dev_data = prepare_data(args, args.dev_data, tokenizer, term='dev')
 
-    # step 4. load model
+    # step 4. load pretrain model
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
-    model_path = os.path.join(args.model_dir, args.model_specific_dir, 'second_stage' + '_' + args.version)
-    model = torch.load(model_path, map_location=device)
+    if args.stage.startswith('one_stage'):  # 加载预训练模型
+        model = MT5ForConditionalGeneration.from_pretrained(args.pretrain_model).to(device)
+    else:  # 加载微调好的模型
+        model_path = os.path.join(args.model_dir, args.model_specific_dir, args.stage + '_' + args.version)
+        model = torch.load(model_path, map_location=device)
 
     if args.data_parallel and torch.cuda.is_available():
         device_ids = range(torch.cuda.device_count())
@@ -365,7 +349,4 @@ if __name__ == '__main__':
 
     # step 5. finetune
     adam = torch.optim.Adam(model.parameters(), lr=args.lr)
-    contrastive_loss = ContrastiveLoss()
-
-    # 第三阶段微调
-    train_model(model, adam, train_data, dev_data, tokenizer, device, args, contrastive_loss)
+    train_model(model, adam, train_data, dev_data, tokenizer, device, args)
