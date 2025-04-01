189d8442cfa18584da4b978afd7dd6ece6c829ce
chenyongming <nealchan@buaa.edu.cn>
2025-04-01 10:59:31 +0800

    case study

--- src/data_analysis/case_study.py
+++ src/data_analysis/case_study.py
@@ -9,6 +9,11 @@
 
 
 from os import path as osp
+import rouge
+import sys
+sys.setrecursionlimit(5000)
+
+
 ROOT_DIR = osp.dirname(osp.dirname(osp.dirname(osp.abspath(__file__))))  # 项目根目录
 THUCNEWS_DIR = osp.join(ROOT_DIR, 'data', 'THUCNews')
 
@@ -16,6 +21,44 @@ PROPOSED_MODEL_FILE = osp.join(THUCNEWS_DIR, 'companies_news_info_v2.test.direct
 CHATGPT4_FILE = osp.join(THUCNEWS_DIR, 'companies_news_info_v2.test.gpt4_infer.txt')
 CHATGLM_FILE = osp.join(THUCNEWS_DIR, 'companies_news_info_v2.test.llm_infer.txt')
 DEEPSEEK_FILE = osp.join(THUCNEWS_DIR, 'companies_news_info_v2.test.deepseek_infer.txt')
+
+
+def compute_rouge(source, target):
+    """计算rouge-1、rouge-2、rouge-l"""
+    source, target = ' '.join(source), ' '.join(target)
+    try:
+        scores = rouge.Rouge().get_scores(hyps=source, refs=target)
+        return scores[0]['rouge-1']['f'] * 0.2 + scores[0]['rouge-2']['f'] * 0.4 + scores[0]['rouge-l']['f'] * 0.4
+    except ValueError:
+        return 0.0
+
+
+def is_in_text_lst(ori_text, text_lst):
+    """判断文本片段是否存在于text_lst中"""
+    for line in text_lst:
+        oracle, text, summary = line.split('\u0001')
+        if ori_text[:20] in text:
+            return summary
+    return ''
+
+
+def compare_rouge(text, proposed_summary, gpt4_summary, glm_summary, ds_summary):
+    """对比rouge"""
+    try:
+        proposed_rouge = compute_rouge(text, proposed_summary) if proposed_summary else 0.0
+
+        gpt4_rouge = compute_rouge(text, gpt4_summary) if gpt4_summary else float('inf')
+        glm_rouge = compute_rouge(text, glm_summary) if glm_summary else float('inf')
+        ds_rouge = compute_rouge(text, ds_summary) if ds_summary else float('inf')
+
+        if proposed_rouge > gpt4_rouge and proposed_rouge > glm_rouge and proposed_rouge > ds_rouge:
+            return True
+    except BaseException as e:
+        print(text)
+        print('-' * 100)
+        print(proposed_summary)
+        raise e
+    return False
 
 
 def test():
@@ -27,7 +70,15 @@ def test():
         glm_lst = [line.strip() for line in f_glm.readlines()]
         ds_lst = [line.strip() for line in f_ds.readlines()]
         total_lst = proposed_lst + gpt4_lst + glm_lst + ds_lst
-        for i in proposed_lst:
+        for line in proposed_lst:
+            proposed_summary, text = line.split('\u0001')
+            gpt4_summary = is_in_text_lst(text, gpt4_lst)
+            glm_summary = is_in_text_lst(text, glm_lst)
+            ds_summary = is_in_text_lst(text, ds_lst)
+            # 指标对比
+            flag = compare_rouge(text, proposed_summary, gpt4_summary, glm_summary, ds_summary)
+            if flag:
+                print({'text': text, 'proposed_summary': proposed_summary, 'gpt4_summary': gpt4_summary, 'glm_summary': glm_summary, 'ds_summary': ds_summary})
 
 
 if __name__ == '__main__':
